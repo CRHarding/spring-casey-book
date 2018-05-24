57f22477adb963988fb4a5a0f6380daedc2ded63
CRHarding <casey.r.harding@gmail.com>
2018-05-24 12:00:37 -0400

    Set up users table

---
+++ spring-boot-caseybook/src/main/resources/db/migration/V1__Create_users_table.sql
@@ -0,0 +1,8 @@
+create table USERS (
+    ID serial,
+    USER_NAME varchar(100) NOT NULL,
+    FIRST_NAME varchar(100),
+    LAST_NAME varchar(100),
+    EMAIL varchar(100),
+    LOCATION varchar(100)
+)
---
+++ spring-boot-caseybook/src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,5 @@
+INSERT INTO USERS (USER_NAME, FIRST_NAME, LAST_NAME, EMAIL, LOCATION)
+VALUES
+    ('CRHarding', 'Casey', 'Harding', 'casey.r.harding@gmail.com', 'New York, New York'),
+    ('RBritt', 'Ryan', 'Brit', 'r.britt@gmail.com', 'New York, New York'),
+    ('Doodeitstom', 'Tom', 'Choe', 't.choe@gmail.com', 'New York, New York');
